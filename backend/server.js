4bc910003ae22a07e2be9c9952fe9ae4c04afcca
Manojit Balasubramanian <110704359+manojitbalasubramanian@users.noreply.github.com>
2024-08-30 19:23:00 +0530

    user disp created

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connecttomongodb from "./db/connecttomongoDB.js";
 
 
@@ -17,7 +20,7 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
-
+app.use("/api/user",userRoutes);
 /*
 app.get("/",(req,res)=>{
     res.send("Hello world!!");
