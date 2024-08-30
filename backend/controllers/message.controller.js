5f9dee9c7b5f1b657d18f03a4675344fe8230c4c
Manojit Balasubramanian <110704359+manojitbalasubramanian@users.noreply.github.com>
2024-08-30 07:30:34 +0530

    send and get message for the user

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -27,10 +27,34 @@ export const sendMessage = async(req,res)=>{
             conversation.message.push(newMessage._id);
         }
 
+        // await conversation.save();
+        // await newMessage.save();
+         await Promise.all([conversation.save(),newMessage.save()]);
+
         res.status(201).json({newMessage})
 
     } catch (error) {
-        console.log("Error in message controller : ",error.message );
+        console.log("Error in sendmessage controller : ",error.message );
+        res.status(500).json({error:"Internal server error"});
+    }
+};
+
+export const getMessage = async(req,res)=>{
+    try {
+         const {id:userToChatId}=req.params;
+         const senderId=req.user._id;
+
+         const conversation = await Conversation.findOne({
+            participants:{$all:[senderId,userToChatId]},
+         }).populate("message");// populate is not reference but it is the actual message
+
+         if(!conversation) return res.status(200).json([])
+
+         const message = conversation.message;
+
+         res.status(200).json(message);
+    } catch (error) {
+        console.log("Error in getmessage controller : ",error.message );
         res.status(500).json({error:"Internal server error"});
     }
 };
